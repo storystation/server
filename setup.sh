fd797638e08265afc9e28cb7205229b719d88c13
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2019-08-20 23:27:25 +0200

    feat(ops): Improve  and optimize provision script

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 export PYTHON_BIN=/home/vagrant/.pyenv/shims
 export PYTHON_VENV_BIN=/home/vagrant/flask/venv/bin
+export VENV_LIB_FOLDER=/home/vagrant/flask/venv/lib/python3.7/site-packages/
 
-if [[ ! -f $PYTHON_VENV_BIN/flask ]]; then
+if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine ] || [ ! -d $VENV_LIB_FOLDER/jwt ]; then
     $PYTHON_BIN/python -m venv venv --copies
     . venv/bin/activate
 
-    $PYTHON_VENV_BIN/pip install -r requirements.txt
+    $PYTHON_BIN/python -m pip install -r requirements.txt
 fi
 
