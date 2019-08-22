8b31d217e1a764b80ed41a0b896acff9a47b2178
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2019-08-22 17:12:22 +0200

    feat(ws): Installed websocket server
    Upgrade the setup tools to configure nginx correctly
    
    update debugging script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ export PYTHON_BIN=/home/vagrant/flask/venv/bin
 export PYTHON_VENV_BIN=/home/vagrant/flask/venv/bin
 export VENV_LIB_FOLDER=/home/vagrant/flask/venv/lib/python3.7/site-packages/
 
-if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine ] || [ ! -d $VENV_LIB_FOLDER/jwt ] || [ ! -d $VENV_LIB_FOLDER/flask_cors ]; then
+if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine ] || [ ! -d $VENV_LIB_FOLDER/jwt ] || [ ! -d $VENV_LIB_FOLDER/flask_cors ] || [ ! -d $VENV_LIB_FOLDER/flask_socketio ]; then
     $PYTHON_BIN/python -m venv venv --copies
     . venv/bin/activate
 
--- src/startup.py
+++ src/startup.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask
 from flask_mongoengine import MongoEngine
 from flask_cors import CORS
+from flask_sockets import Sockets
 
 from Routes import Auth, Story
 
@@ -8,7 +11,9 @@ from Routes import Auth, Story
 app = Flask(__name__)
 app.config.from_json('config_default.json')
 app.config.from_json('config_user.json', True)
+app.config['DEBUG'] = os.environ.get('DEBUG', False)
 cors = CORS(app, send_wildcard=True)
+sockets = Sockets(app)
 
 db = MongoEngine(app)
 
@@ -19,3 +24,21 @@ app.register_blueprint(Story.bp)
 @app.route('/')
 def hello_world():
     return "Hello, World !"
+
+
+@sockets.route('/ws/test')
+def echo_socket(socket):
+    while not socket.closed:
+        message = socket.receive()
+        socket.send(message)
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    server = pywsgi.WSGIServer(('', 3333), app, handler_class=WebSocketHandler, log=logging.Logger)
+    server.serve_forever()
