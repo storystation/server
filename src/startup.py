2f515946c783a4c57a732ce58d419aa64b986b4b
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2019-08-22 09:36:38 +0200

    feat(cors): setup cors to allow everyhting
    Update server py dependencies

--- src/startup.py
+++ src/startup.py
@@ -1,13 +1,14 @@
-import sys
-
 from flask import Flask
 from flask_mongoengine import MongoEngine
+from flask_cors import CORS
 
 from Routes import Auth
 
 app = Flask(__name__)
 app.config.from_json('config_default.json')
 app.config.from_json('config_user.json', True)
+cors = CORS(app, send_wildcard=True)
+
 db = MongoEngine(app)
 
 app.register_blueprint(Auth.bp)
