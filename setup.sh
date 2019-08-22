243ca5ea07f25ba358fef5b8007316bfe7e2132e
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2019-08-22 09:36:38 +0200

    feat(cors): setup cors to allow everyhting
    Update server py dependencies

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ export PYTHON_BIN=/home/vagrant/.pyenv/shims
 export PYTHON_VENV_BIN=/home/vagrant/flask/venv/bin
 export VENV_LIB_FOLDER=/home/vagrant/flask/venv/lib/python3.7/site-packages/
 
-if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine ] || [ ! -d $VENV_LIB_FOLDER/jwt ]; then
+if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine ] || [ ! -d $VENV_LIB_FOLDER/jwt ] || [ ! -d $VENV_LIB_FOLDER/flask_cors ]; then
     $PYTHON_BIN/python -m venv venv --copies
     . venv/bin/activate
 
