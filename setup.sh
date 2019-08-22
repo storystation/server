607dd531647da8f4a5295e95115b7bd8d7ecf5dc
Quentin B <32564163+QuentinBouvier@users.noreply.github.com>
2019-08-22 09:49:56 +0200

    feat(ops): Fix requirements and minor issue in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export PYTHON_BIN=/home/vagrant/.pyenv/shims
+export PYTHON_BIN=/home/vagrant/flask/venv/bin
 export PYTHON_VENV_BIN=/home/vagrant/flask/venv/bin
 export VENV_LIB_FOLDER=/home/vagrant/flask/venv/lib/python3.7/site-packages/
 
@@ -8,6 +8,6 @@ if [ ! -f $PYTHON_VENV_BIN/flask ] || [ ! -d $VENV_LIB_FOLDER/flask_mongoengine 
     $PYTHON_BIN/python -m venv venv --copies
     . venv/bin/activate
 
-    $PYTHON_BIN/python -m pip install -r requirements.txt
+    $PYTHON_VENV_BIN/python -m pip install -r requirements.txt
 fi
 
