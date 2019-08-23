7c578cf6bf193253d5c26daa2f2fab1160f68df0
MartyHemart <marty.hemart@hotmail.fr>
2019-08-23 07:04:28 -0700

    feat(story): Read to Story

--- src/Routes/Story.py
+++ src/Routes/Story.py
@@ -4,7 +4,13 @@ from Controller import StoryController
 bp = Blueprint('story', __name__, url_prefix='/story')
 
 @bp.route('/', methods=['POST'])
-def create_story(): return StoryController.store(req=request)
+def create_story(): 
+    return StoryController.store(req=request)
 
 @bp.route('/', methods=['GET'])
-def list_story(): return StoryController.liste(req=request)
+def list_story(): 
+    return StoryController.liste(req=request)
+
+@bp.route('/<id>', methods=['GET'])
+def read_story(id): 
+    return StoryController.read(req=request, id=id)
